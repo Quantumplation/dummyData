74ef6324c39023054ac33ef112093c7f8204cb59
Simon Gellis <sag470@nyu.edu>
2014-06-17 13:56:56 -0400

    Mocked out a "data depot" system which has access to the data reference graph

---
+++ DataDepotUnitOfWork/TDataDepotUnitOfWork.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataDepotUnitOfWork
+{
+    public class Foo : Persistable
+    {
+        public Bar Bar { get; set; }
+    }
+
+    public class Bar : Persistable
+    {
+        public Baz Baz { get; set; }
+    }
+
+    public class Baz : Persistable
+    {
+        public string Name { get; set; }
+    }
+
+
+    public class FooDepot : IDataDepot
+    {
+        public Foo Foo { get; private set; }
+
+        public void Populate(UnitOfWork uow, DataDepotRepo depotRepo)
+        {
+            Foo = uow.Add(new Foo
+            {
+                Bar = depotRepo.Data<BarDepot>().Bar
+            });
+        }
+    }
+
+    public class BarDepot : IDataDepot
+    {
+        public Bar Bar { get; private set; }
+
+        public void Populate(UnitOfWork uow, DataDepotRepo depotRepo)
+        {
+            Bar = uow.Add(new Bar
+            {
+                Baz = uow.Add(new Baz
+                {
+                    Name = "Original unmodified name"
+                })
+            });
+        }
+    }
+    
+    [TestClass]
+    public class TDataDepotUnitOfWork
+    {
+        private UnitOfWork _uow;
+        private DataDepotRepo _data;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _uow = new UnitOfWork();
+            _data = new DataDepotRepo(_uow);
+            _data.Register<BarDepot>();
+            _data.Register<FooDepot>();
+        }
+
+        [TestMethod]
+        public void ShouldChangeUnitOfWorkContentsWhenModified()
+        {
+            var baz = _data.Data<BarDepot>().Bar.Baz;
+            baz.Name = "different name than it had before";
+
+            Assert.AreEqual(baz.Name, _uow.Get<Baz>(baz.Id).Name);
+        }
+
+        [TestMethod]
+        public void ShouldCopyChangesMadeOnSave()
+        {
+            var depotBaz = _data.Data<BarDepot>().Bar.Baz;
+            var uowBaz = _uow.Get<Baz>(depotBaz.Id);
+            Assert.AreNotSame(depotBaz, uowBaz);
+
+            uowBaz.Name = "different name";
+            Assert.AreNotEqual(uowBaz.Name, depotBaz.Name);
+
+            _uow.Save(uowBaz);
+            Assert.AreEqual(uowBaz.Name, depotBaz.Name);
+        }
+    }
+}
---
+++ DataDepotUnitOfWork/UnitOfWork.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDepotUnitOfWork
+{
+    public class UnitOfWork
+    {
+        private readonly IDictionary<Tuple<Type, int>, Persistable> _persistables;
+
+        public UnitOfWork()
+        {
+            _persistables = new Dictionary<Tuple<Type, int>, Persistable>();
+        }
+
+        public T Get<T>(int id) where T : Persistable, new()
+        {
+            var origP = (T)GetOriginal(typeof (T), id);
+            var newP = new T {Id = id};
+            foreach (var prop in typeof (T).GetProperties())
+            {
+                object newValue;
+                if (typeof (Persistable).IsAssignableFrom(prop.PropertyType))
+                {
+                    var propId = ((Persistable) prop.GetValue(origP)).Id;
+                    newValue = prop.PropertyType.GetConstructor(new Type[0]).Invoke(null);
+                    prop.PropertyType.GetProperty("Id", typeof (int)).SetValue(newValue, propId);
+                }
+                else
+                {
+                    newValue = prop.GetValue(origP);
+                }
+                prop.SetValue(newP, newValue);
+            }
+            return newP;
+        }
+
+        public void Save<T>(T persistable) where T : Persistable, new()
+        {
+            T oldP;
+            if (persistable.Id == 0)
+            {
+                oldP = new T();
+                SaveOriginal(oldP);
+                persistable.Id = oldP.Id;
+            }
+            else
+            {
+                oldP = (T) GetOriginal(typeof (T), persistable.Id);
+            }
+
+            foreach (var prop in persistable.GetType().GetProperties())
+            {
+                if (typeof (Persistable).IsAssignableFrom(prop.PropertyType))
+                {
+                    var id = ((Persistable) prop.GetValue(persistable)).Id;
+                    prop.SetValue(oldP, GetOriginal(prop.PropertyType, id));
+                }
+                else
+                {
+                    prop.SetValue(oldP, prop.GetValue(persistable));
+                }
+            }
+        }
+
+        public T Add<T>(T persistable) where T : Persistable
+        {
+            SaveOriginal(persistable);
+            return persistable;
+        }
+
+        private Persistable GetOriginal(Type type, int id)
+        {
+            return _persistables[Tuple.Create(type, id)];
+        }
+
+        private void SaveOriginal(Persistable persistable)
+        {
+            var id = _persistables.Count + 1;
+            persistable.Id = id;
+            _persistables.Add(Tuple.Create(persistable.GetType(), id), persistable);
+        }
+    }
+
+    public class DataDepotRepo
+    {
+        private readonly UnitOfWork _uow;
+        private readonly IDictionary<Type, IDataDepot> _depots;
+
+        public DataDepotRepo(UnitOfWork uow)
+        {
+            _uow = uow;
+            _depots = new Dictionary<Type, IDataDepot>();
+        }
+
+        public void Register<T>() where T : IDataDepot, new()
+        {
+            if (_depots.ContainsKey(typeof (T)))
+                return;
+
+            var depot = new T();
+            depot.Populate(_uow, this);
+            _depots.Add(typeof(T), depot);
+        }
+
+        public T Data<T>() where T : IDataDepot
+        {
+            return (T) _depots[typeof (T)];
+        }
+
+    }
+
+    public interface IDataDepot
+    {
+        void Populate(UnitOfWork uow, DataDepotRepo depotRepo);
+    }
+
+    public class Persistable
+    {
+        public int Id { get; set; }
+    }
+}
